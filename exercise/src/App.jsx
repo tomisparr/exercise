260e51f0555ec43a6fc7fad5389ac100504b2d0d
Bunmi Adekunle <bumspin@gmail.com>
2024-04-24 17:44:46 +0100

    exercise

--- exercise/src/App.jsx
+++ exercise/src/App.jsx
@@ -1,26 +1,32 @@
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './Component/Header'
+import Content from './Component/Content'
+import Footer from './Component/Footer'
 
+function App() { 
+    const course = 'Half Stack application development';
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+    
   return (
-    <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
+    <>
+    <Header course = {course}/>
+
+    <Content part1 = {part1} part2 = {part2} part3 = {part3} exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3} />
+    
+    
+    
+    <Footer exercises = {exercises1 + exercises2 + exercises3}/>    
+
+     
+    </>
   )
 }
 
---
+++ exercise/src/Component/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = (props) => {
+
+    return (
+              
+         <p>
+            Number of exercises {props.exercises};
+            </p>
+        
+        
+        
+
+    )
+}
+export default Footer 
